169b345d58c6b96fab7dfc60edeb2cd5b0b0eaa7
akinloludavid <akinloludavid2727@gmail.com>
2020-09-07 09:53:29 -0700

    updated serviceworker.js

--- weather/serviceWorker.js
+++ weather/serviceWorker.js
@@ -1,4 +1,5 @@
 const cacheArr = ['/', './css/*'];
+const CACHE_NAME = 'cache-v1'
 self.addEventListener('install', (event)=>{
     console.log('worker is installed');
     event.waitUntil(
@@ -6,5 +7,32 @@ self.addEventListener('install', (event)=>{
             console.log("Opened cache");
             cache.addAll(cacheArr).then(() => self.skipWaiting());
         })
+    );
+});
+
+
+self.addEventListener("activate", (event)=>{
+    event.waitUntil(
+        caches.keys().then((cacheNames) =>{
+            return Promise.all(
+                cacheNames.map((cacheName )=>{
+                    if (CACHE_NAME !== cacheName){
+                        return caches.delete(cacheName);
+                    }
+                })
+            )
+        })
+    )
+})
+
+self.addEventListener('fetch', (event)=>{
+    event.respondWith(
+        caches.match(event.request)
+            .then((response)=>{
+                if(response){
+                    return response;
+                }
+                return fetch(event.request).catch(()=>() => caches.match(event.request));
+            })
     )
 })

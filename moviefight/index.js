c2d5b45efb6de0207a150f1ae46fc9235ee6641b
akinloludavid <akinloludavid2727@gmail.com>
2021-01-21 22:25:02 +0900

    update

--- moviefight/index.js
+++ moviefight/index.js
@@ -55,7 +55,7 @@ let leftMovie;
 let rightMovie;
 
 const onMovieSelect = async (movie, summaryElement, side) => {
-    const response = await axios.get('http://www.omdbapi.com/',{
+    const response = await axios.get('https://www.omdbapi.com/',{
         params: {
             apikey: '8b9c48c2',
 
